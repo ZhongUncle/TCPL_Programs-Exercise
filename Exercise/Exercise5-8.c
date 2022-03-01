934bab41e897bccff28f4b73b8f0a7c9f3ef3774
ZhongUncle <2020738339@qq.com>
2022-03-01 16:52:42 +0800

    modified:   Exercise5-8.c

--- Exercise/Exercise5-8.c
+++ Exercise/Exercise5-8.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int day_of_year(int year, int month, int day);
-void month_day(int year, int yearday, int *pmonth, int *pday);
+int month_day(int year, int yearday, int *pmonth, int *pday);
 
 int main()
 {
@@ -9,8 +9,21 @@ int main()
     int pmonth, pday;
     
     dayOfYear = day_of_year(2000, 3, 18);
-    month_day(2000, 40, &pmonth, &pday);
-    printf("3/18/2000 is %d day of year.\n", dayOfYear);
+    
+    if (dayOfYear == -1) {
+        printf("Input month error.\n");
+    }else if (dayOfYear == -2){
+        printf("Input day error.\n");
+    }else{
+        printf("3/18/2000 is %d day of year.\n", dayOfYear);
+    }
+    
+    if (month_day(2000, 400, &pmonth, &pday) == 1){
+        printf("Month is %d, day is %d.\n", pmonth, pday);
+    }else{
+        printf("The year day is worry!\n");
+    }
+    
     
     return 0;
 }
@@ -26,6 +39,13 @@ int day_of_year(int year, int month, int day)
     int i, leap;
     
     leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    
+    if (month < 1 || month > 12){
+        return -1;
+    }else if (day < 0 || day > daytab[leap][month]){
+        return -2;
+    }
+    
     for (i = 1; i < month; i++){
         day += daytab[leap][i];
     }
@@ -33,14 +53,20 @@ int day_of_year(int year, int month, int day)
 }
 
 /* month_day: set month, day from day of year */
-void month_day(int year, int yearday, int *pmonth, int *pday)
+int month_day(int year, int yearday, int *pmonth, int *pday)
 {
     int i, leap;
     
     leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    
+    if (yearday < 0 || (leap == 0 && yearday > 365) || (leap == 1 && yearday > 366)){
+        return 0;
+    }
+    
     for (i = 1; yearday > daytab[leap][i]; i++){
         yearday -= daytab[leap][i];
     }
     *pmonth = i;
     *pday = yearday;
+    return 1;
 }
